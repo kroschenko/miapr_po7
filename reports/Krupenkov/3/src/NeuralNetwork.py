0ea1ecf3d2c35debe7aa0d3ec59c98c01cc206aa
FalseR20 <kno3.2002@gmail.com>
2021-10-18 21:14:56 +0300

    edit lab3: edit NeuralNetwork.py, not works

--- reports/Krupenkov/3/src/NeuralNetwork.py
+++ reports/Krupenkov/3/src/NeuralNetwork.py
@@ -1,5 +1,4 @@
 import numpy as np
-from pprint import pprint
 
 
 def sigmoid(s):  # sigmoid
@@ -19,54 +18,60 @@ def d_linear(y):
 
 
 class Layer:
-    def __init__(self, weigths: np.ndarray, threshold: float = 0.5, f_act=linear, d_f_act=d_linear):
-        self.w = weigths
-        self.t = threshold
+    def __init__(self, lens: tuple[int, int],
+                 f_act=linear, d_f_act=d_linear):
+        self.w = np.random.uniform(0, 0.5, lens)
+        self.t = np.random.uniform(0, 0.5)
         self.f_act = f_act
         self.d_f_act = d_f_act
 
     def go(self, x: np.ndarray) -> np.ndarray:
-        self.s = np.dot(x, self.w)
-        y = self.f_act(self.s)
-        return y
+        self.x = x
+        self.s: np.ndarray = np.dot(x, self.w) - self.t
+        self.y = self.f_act(self.s)
+        return self.y
 
-    def error_propagation(self, error: np.ndarray):
-        self.error = error * self.w * self.d_f_act(self.s)
-        print(error, self.w, self.d_f_act(self.s), self.error)
-        return self.error
+    def back_propagation(self, error: np.ndarray, alpha=0.5):
+        # print(self.w, error, self.d_f_act(self.s), sep='\n', end='\n\n')
+        error_later = np.dot(self.w * self.d_f_act(self.s.transpose()), error)
 
+        for j in range(self.w.shape[1]):
+            for i in range(self.w.shape[0]):
+                gamma = alpha * error[j] * self.d_f_act(self.s[j])
+                self.w[i][j] -= gamma * self.x[i]
+                self.t += gamma
+
+        return error_later
 
 
 class NeuralNetwork:
-    def __init__(self, layers: list[Layer]):
-        # self.layer_count: int = len(layers)
-        self.layers: list[Layer] = layers
+    def __init__(self, layers):
+        self.layers = layers
 
     def go(self, x: np.ndarray) -> np.ndarray:
         for layer in self.layers:
             x = layer.go(x)
         return x
 
-    def learn(self, x: np.ndarray, e: float):
+    def learn(self, x: np.ndarray, e, alpha=0.1):
         result = self.go(x)
-        delta: float = result - e
-
-        delta_array = np.array([delta], float)
+        delta = result - e
         for layer in reversed(self.layers):
-            delta_array = layer.error_propagation(delta_array)
-
-
+            delta = layer.back_propagation(delta, alpha=0.1)
 
 
 def main():
-    layer_input = Layer(weigths=np.array([[0.1, 0.2], [0.3, 0.4]]))
-    layer_hidden = Layer(weigths=np.array([0.5, 0.6]))
-    x = np.array([1., 2.])
-
-
-    nn = NeuralNetwork([layer_input, layer_hidden])
-    # print(nn.go(x))
-    nn.learn(x, 1.)
+    l1 = Layer(lens=(2, 3), f_act=sigmoid, d_f_act=d_sigmoid)
+    l2 = Layer(lens=(3, 1), f_act=linear, d_f_act=d_linear)
+    nn = NeuralNetwork([l1, l2])
+    x = np.array([[0, 0], [1, 0], [1, 0], [1, 1]], float)
+    for i in range(1):
+        nn.learn(x=np.array([0, 0], float), e=0.0)
+        nn.learn(x=np.array([0, 1], float), e=1.0)
+        nn.learn(x=np.array([1, 0], float), e=1.0)
+        nn.learn(x=np.array([1, 1], float), e=0.0)
+    for el in x:
+        print(nn.go(el))
 
 
 if __name__ == '__main__':
--- reports/Krupenkov/3/src/main2.py
+++ reports/Krupenkov/3/src/main2.py
@@ -1,5 +1,164 @@
-from NeuralNetwork import *
 import numpy as np
+from pprint import pprint
+from random import uniform
+from typing import List
 
 
+def function(x):
+    return 0.1 * np.cos(0.3 * x) + 0.08 * np.sin(0.3 * x)
 
+
+def activation(s):  # sigmoid
+    return 1 / (1 + np.exp(-s))
+
+
+def d_activation(y):  # sigmoid'
+    return y * (1 - y)
+
+
+class NeuralNetwork:
+    x_amount: int
+    h_amount: int
+    divider: float
+    t_learning: int
+    t_testing: int
+    speed: float
+    epoch_amount: int
+
+    x: List[float]
+    e: List[float]
+    wx: List[List[float]]
+    Th: float
+    Ty: float
+    h: List[float]
+    sh: float
+    wh: List[float]
+    y: float
+    sy: float
+    y_delta: float
+    h_delta: float
+
+    def __init__(self, x_amount, h_amount, divider, t_max, speed, epoch_amount, t_testing):
+        self.x_amount = x_amount
+        self.h_amount = h_amount
+        self.divider = divider
+        self.t_learning = t_max
+        self.speed = speed
+        self.epoch_amount = epoch_amount
+        self.t_testing = t_testing
+
+        self.wx = [[uniform(-0.1, 0.1) for _ in range(self.h_amount)] for _ in range(self.x_amount)]
+        self.wh = [uniform(-0.1, 0.1) for _ in range(self.h_amount)]
+        # self.Th = uniform(-0.1, 0.1)
+        # self.Ty = uniform(-0.1, 0.1)
+        self.Th, self.Ty = 0, 0
+
+
+    def calculating(self, x):
+        self.sh = 0
+        self.h = []
+        for j in range(self.h_amount):
+            for i, xi in enumerate(x):
+                self.sh += self.wx[i][j] * xi
+            self.sh -= self.Th
+            self.h.append(activation(self.sh))
+        self.sy = 0
+        for j in range(self.h_amount):
+            self.sy += self.h[j] * self.wh[j]
+        self.y = self.sy - self.Ty
+
+    def learning(self):
+        self.x = [i / self.divider for i in range(self.t_learning + self.x_amount)]
+        self.e = self.x[self.x_amount:] + [(self.t_learning + self.x_amount) / self.divider]
+        self.x = [function(x) for x in self.x]
+        self.e = [function(y) for y in self.e]
+
+        print(' N:      эталонное значение     полученное значение                 разница')
+
+        for epoch in range(self.epoch_amount):
+            print(f'Epoch: {epoch}')
+            for t in range(self.t_learning):
+                self.calculating(self.x[t:t + self.x_amount])
+
+                self.y_delta = self.y - self.e[t]
+                print(f'{t:2}: {self.e[t]:23} {self.y:23} {self.y_delta:23}')
+
+                self.h_delta = 0
+                for j in range(self.h_amount):
+                    self.h_delta += self.y_delta * self.wh[j]
+
+                for j in range(self.h_amount):
+                    self.wh[j] -= self.speed * self.y_delta * self.y
+                    self.Ty += self.speed * self.y_delta
+
+                for j in range(self.h_amount):
+                    for i in range(self.x_amount):
+                        self.wx[i][j] -= self.speed * self.h_delta * d_activation(self.h[j]) * self.h[j]
+                        self.Th += self.speed * self.h_delta * d_activation(self.y)
+
+        print('Тестирование на новом участке с использованием предсказанных значений')
+        print(' N:      эталонное значение     полученное значение                 разница')
+
+    def testing(self):
+        self.x = [i / self.divider for i in range(self.t_learning, self.t_learning + self.x_amount)]
+        self.e = [i / self.divider for i in range(self.t_learning + self.x_amount,
+                                                  self.t_learning + self.x_amount + self.t_testing)]
+        self.x = [function(x) for x in self.x]
+        self.e = [function(y) for y in self.e]
+        square_error = 0
+
+        for t in range(self.t_testing):
+            self.calculating(self.x)
+            self.y_delta = self.y - self.e[t]
+            square_error += self.y_delta ** 2
+            print(f'{t:2}: {self.e[t]:23} {self.y:23} {self.y_delta:23}')
+            self.x = self.x[1:] + [self.y]
+
+        return square_error
+
+    def get_weights(self):
+        print(f'\nwx: ')
+        pprint(self.wx, width=120, compact=True)
+
+        print(
+            f'''Th:
+{self.Th}
+wh: 
+{self.wh}
+Ty: 
+{self.Ty}
+''')
+        return self.wx, self.Th, self.wh, self.Ty
+
+    def set_weights(self, wx, Th, wh, Ty):
+        self.wx, self.Th, self.wh, self.Ty = wx, Th, wh, Ty
+
+
+def main():
+    x_amount = 10
+    h_amount = 4
+    divider = 10
+    t_learning = 30
+    t_testing = 15
+    speed = 0.2
+    epoch_amount = 1
+
+    i = 0
+    while True:
+        nn = NeuralNetwork(x_amount, h_amount, divider, t_learning, speed, epoch_amount, t_testing)
+        try:
+            nn.learning()
+        finally:
+            cum = nn.testing()
+            print(f'Новая нейронная сеть №{i}: {cum}')
+            i += 1
+            if cum < 0.0001:
+                for_coping = nn.get_weights()
+                print('For coping:')
+                pprint(for_coping, width=120, compact=True)
+                break
+            del nn
+
+
+if __name__ == '__main__':
+    main()
