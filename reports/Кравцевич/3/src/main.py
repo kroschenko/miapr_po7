8dc8f26aa5f50eb45d30399976bf89864b27b36c
KrawGleb <krawcevitsch@gmail.com>
2021-11-07 14:39:03 +0300

    Add Lab4

--- reports/Кравцевич/3/src/main.py
+++ reports/Кравцевич/3/src/main.py
@@ -1,22 +1,20 @@
+import activation_functions
+from layer import Layer
 import math
-import random
 import pandas as pd
 import matplotlib.pyplot as plt
 
-EXCPECTED_ERROR = 1e-5
+EXPECTED_ERROR = 1e-6
 INPUT_SIZE = 6
 HIDDEN_LAYER_SIZE = 2
-FUNC_STEP = 0.1
-TRAINING_STEP = 0.5
-weights = {}
-T = {}
+FUNC_STEP = 0.5
 
-error_changes = []
+input_layer = Layer(INPUT_SIZE, HIDDEN_LAYER_SIZE, activation_functions.Sigmoid())
+hidden_layer = Layer(HIDDEN_LAYER_SIZE, 1, activation_functions.Linear())
 
 
 def get_func_value(x):
-	a, b, d, c = 0.3, 0.1, 0.06, 0.1
-	return a * math.cos(b * x) + c * math.sin(d * x)
+	return 0.3 * math.cos(0.1 * x) + 0.06 * math.sin(0.1 * x)
 
 
 def get_training_data() -> list:
@@ -40,110 +38,63 @@ def get_training_data() -> list:
 	return training_data
 
 
-def train_model(training_data):
-	sum_error = 1
+def train(training_data):
+	epoch = 0
+	error_points = []
 
-	while (abs(sum_error) > EXCPECTED_ERROR):
-		sum_error = 0
+	while True:
+		epoch += 1
+		sum = 0
 		for sample in training_data:
-			data = sample[0]
-			expected_result = sample[1]
+			sum += train_model(sample[0], sample[1])
 
-			results = {}
-			results[0] = data  # input data
-			results[1] = get_layer_value(data)  # hidden layer
-			results[2] = get_layer_value(results[1], 2, 1)[0]  # output layer
+		error_points.append(sum)
+		print(sum)
 
-			error = results[2] - expected_result
-
-			prev_errors = []
-			for layer_number in range(2, 0, -1):
-				for y_index in range(len(results[layer_number - 1])):
-					prev_error = error * results[layer_number - 1][y_index] * (1 - results[layer_number - 1][y_index]) * \
-								 weights[layer_number][y_index][0]
-					prev_errors.append(prev_error)
-				sum_error += 0.5 * sum([x ** 2 for x in prev_errors])
-				change_weights(prev_errors, layer_number, results[layer_number], results[layer_number - 1])
-
-		error_changes.append(sum_error)
-		print(sum_error)
+		if sum < EXPECTED_ERROR:
+			error_points = error_points[:50]
+			plt.plot(range(len(error_points)), error_points)
+			plt.savefig('errors')
+			print('Epoch: ', epoch)
+			break
 
 
-def change_weights(errors, layer_number, values, current):
-	global TRAINING_STEP
-	if type(errors) != list:
-		errors = [errors]
-	if type(values) != list:
-		values = [values]
+def train_model(train_sample, expected_result):
+	input_layer.set_input_values(train_sample)
+	hidden_layer.set_input_values(input_layer.output_values)
 
-	for index, weight in enumerate(weights[layer_number]):
-		for i, w in enumerate(weight):
-			weights[layer_number][index][i] -= TRAINING_STEP * errors[i] * values[i] * (1 - values[i]) * current[index]
+	error = hidden_layer.output_values[0] - expected_result
+	input_layer.init_errors(hidden_layer.init_errors([error]))
 
-			T[layer_number][i] += TRAINING_STEP * errors[i] * values[i] * (1 - values[i])
+	return error ** 2
 
 
-def get_layer_value(input_data, layer_number=1, layer_size=HIDDEN_LAYER_SIZE):
-	if layer_number not in weights:
-		init_weights(layer_number, len(input_data), layer_size)
+def prediction(sample):
+	input_layer.set_input_values(sample)
+	hidden_layer.set_input_values(input_layer.output_values)
 
-	sums = [get_sum(input_data, layer_number, i + 1, ) for i in range(layer_size)]
-
-	values = get_values(sums)
-
-	return values
-
-
-def get_values(sums):
-	output = []
-	for S in sums:
-		output.append(1 / (1 + math.exp(-1 * S)))
-
-	return output
-
-
-def get_sum(input_data, layer_number, sum_number):
-	sum = 0
-
-	for i in range(len(input_data)):
-		sum += input_data[i] * weights[layer_number][i][sum_number - 1]
-	sum -= T[layer_number][sum_number - 1]
-
-	return sum
-
-
-def init_weights(number, input_size, layer_size):
-	weight_matrix = []
-
-	for _ in range(input_size):
-		weight_matrix.append([random.uniform(0, 1) for __ in range(layer_size)])
-	t = [random.uniform(0, 1) for _ in range(layer_size)]
-
-	weights[number] = weight_matrix
-	T[number] = t
-
-
-def predict(vector):
-	return get_layer_value(get_layer_value(vector), 2, 1)[0]
+	return hidden_layer.output_values[0]
 
 
 if __name__ == '__main__':
 	training_data = get_training_data()
-	train_model(training_data)
+	train(get_training_data())
 
 	right_results, results, errors = [], [], []
 
 	for line in training_data:
 		test_vector = line[0]
 		result = line[1]
-		prediction = predict(test_vector)
+		predict = prediction(test_vector)
 
 		right_results.append(result)
-		results.append(prediction)
-		errors.append(abs(result - prediction))
+		results.append(predict)
+		errors.append(abs(result - predict))
 
 	table = pd.DataFrame({
 		'Right': pd.Series(right_results),
 		'Received': pd.Series(results),
 		'Errors': pd.Series(errors)
 	})
+
+	print(table)
--- reports/Кравцевич/4/src/main.py
+++ reports/Кравцевич/4/src/main.py
@@ -2,8 +2,9 @@ import activation_functions
 from layer import Layer
 import math
 import pandas as pd
+import matplotlib.pyplot as plt
 
-EXPECTED_ERROR = 1e-5
+EXPECTED_ERROR = 1e-6
 INPUT_SIZE = 6
 HIDDEN_LAYER_SIZE = 2
 FUNC_STEP = 0.5
@@ -12,7 +13,7 @@ input_layer = Layer(INPUT_SIZE, HIDDEN_LAYER_SIZE, activation_functions.Sigmoid(
 hidden_layer = Layer(HIDDEN_LAYER_SIZE, 1, activation_functions.Linear())
 
 
-def get_func_value(x):
+def get_func_value(x: float) -> float:
 	return 0.3 * math.cos(0.1 * x) + 0.06 * math.sin(0.1 * x)
 
 
@@ -37,41 +38,41 @@ def get_training_data() -> list:
 	return training_data
 
 
-def train(training_data):
-	epos = 0
+def train(training_data: list) -> None:
+	epoch = 0
+	error_points = []
+
 	while True:
-		epos += 1
+		epoch += 1
 		sum = 0
 		for sample in training_data:
 			sum += train_model(sample[0], sample[1])
 
+		error_points.append(sum)
 		print(sum)
-		if sum > 100:
-			print(input_layer.weights)
-			print(hidden_layer.weights)
 
-			input()
 		if sum < EXPECTED_ERROR:
-			print('Number: ', epos)
+			error_points = error_points
+			plt.plot(range(len(error_points)), error_points)
+			plt.savefig('errors')
+			print('Epoch: ', epoch)
 			break
 
 
-def train_model(train_sample, expected_result):
-	global input_layer
-	global hidden_layer
-
+def train_model(train_sample: list, expected_result: float) -> float:
 	input_layer.set_input_values(train_sample)
 	hidden_layer.set_input_values(input_layer.output_values)
 
 	error = hidden_layer.output_values[0] - expected_result
-	errors = input_layer.init_errors(hidden_layer.init_errors([error]))
+	input_layer.init_errors(hidden_layer.init_errors([error]))
 
 	return error ** 2
 
 
-def prediction(sample):
+def prediction(sample: list) -> float:
 	input_layer.set_input_values(sample)
 	hidden_layer.set_input_values(input_layer.output_values)
+
 	return hidden_layer.output_values[0]
 
 
